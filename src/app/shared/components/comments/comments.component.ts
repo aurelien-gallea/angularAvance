5fa2a001d496fc11193f229baf44f75a3cbff5ca
a.gallea <a.gallea@transcausse.com>
2024-01-31 11:52:24 +0100

    ajout des animations complexes

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,47 +1,106 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 import { Comment } from '../../../core/models/Comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Person } from '../../../core/models/Person.model';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+} from '@angular/animations';
+import { flashAnimation } from '../../animations/flash.animation';
+import { slideAndFadeAnimation } from '../../animations/slide-and-fade.animation';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
   animations: [
-    trigger('listItem', [
-      state('default', style({
-        'transform' : 'scale(1)',
-        'background-color' : 'white',
-        'z-index' : 1
-      })),
-      state('active', style({
-        'transform' : 'scale(1.05)',
-        'background-color': 'rgb(201, 157,242)',
-        'z-index' : 2
-      })),
-      // transition('default <=> active')
-      transition('default => active', [
-        animate('100ms ease-in-out')
-      ]),
-      transition('active => default', [
-        animate('500ms ease-in-out')
-      ]),
-      // depuis le vide vers n'import quel autre état (ajout de quelque chose)
-      transition('void => *', [
-        style({
-          'transform': 'translateX(-100%)',
-          'opacity' : 0,
-          'background-color' : 'rgb(201, 157,242)',
-        }),
-        animate('250ms ease-out', style({
-          'transform': 'translateX(0)',
-          'opacity' : 1,
-          'background-color' : 'white',
-        }))
+    // element parent
+    trigger('list', [
+      transition(':enter', [
+        query('@listItem', [
+          stagger(100, [
+            animateChild() // déclencheur des animations enfants
+          ])
+        ])
       ])
-    ])
-  ]
+    ]),
+    // element enfant
+    trigger('listItem', [
+      state(
+        'default',
+        style({
+          transform: 'scale(1)',
+          'background-color': 'white',
+          'z-index': 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201, 157,242)',
+          'z-index': 2,
+        })
+      ),
+      // transition('default <=> active')
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')]),
+      // depuis le vide vers n'import quel autre état (ajout de quelque chose) on peut aussi utiliser ':enter' au lieu de 'void => *'
+      transition('void => *', [
+        query('.comment-text, .comment-date', [
+          style({
+            opacity: 0,
+          }),
+        ]),
+       useAnimation(slideAndFadeAnimation, {
+        params: {
+          time: '1000ms',
+          startColor: 'rgb(201, 157, 242)'
+        }
+       }),
+        // parallélisme d'animation
+        group([
+          useAnimation(flashAnimation, {
+            params: {
+              time : '1000ms',
+              flashColor :'rgb(249, 179, 111)'
+            }
+          }),
+          query('.comment-text', [
+            animate(
+              '250ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+          query('.comment-date', [
+            animate(
+              '500ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class CommentsComponent {
   @Input() comments!: Comment[]; // on reçoit une propriété depuis un attribut html du parent
@@ -52,12 +111,12 @@ export class CommentsComponent {
   persons: Person[] = [];
   person: Person = new Person();
   // listItemAnimationState: 'default' |'active' = 'default';
-  animationStates : { [key: number]: 'default' | 'active'} = {};
+  animationStates: { [key: number]: 'default' | 'active' } = {};
 
   constructor(private formBuilder: FormBuilder) {
     for (let index in this.comments) {
       this.animationStates[index] = 'default';
-  }
+    }
   }
 
   ngOnInit() {
@@ -82,18 +141,18 @@ export class CommentsComponent {
       return;
     }
     const newComment = new Comment();
-    const maxId = Math.max(...this.comments.map(comment => comment.id));
-    
+    const maxId = Math.max(...this.comments.map((comment) => comment.id));
+
     newComment.comment = this.commentCtrl.value;
     const date = new Date();
-    
-    newComment.createdDate = date.toUTCString() ;
-    newComment.id = maxId+1;
+
+    newComment.createdDate = date.toUTCString();
+    newComment.id = maxId + 1;
     this.comments.unshift(newComment);
-        
+
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
-    
+
     console.log(newComment);
   }
 
@@ -101,7 +160,7 @@ export class CommentsComponent {
     this.animationStates[index] = 'active';
   }
 
-  onMouseOut(index : number) {
+  onMouseOut(index: number) {
     this.animationStates[index] = 'default';
   }
 }
